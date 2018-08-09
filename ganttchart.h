3a11a15e251cc41d2b3ba155df40847d23bf5191
robyjacob <robyjacob1998@gmail.com>
2018-08-09 20:26:08 +0530

    merged ganttchart.h to master

--- ganttchart.h
+++ ganttchart.h
@@ -2,5 +2,33 @@
 #define GANTTCHART
 
 #include "fcfs.h"
+#include<stdio.h>
+#include<string.h>
+
+void print(Done *dprocess){
+  int size = 0;
+  for (int i = 0; ; i++) {
+    if (strcmp(dprocess[i].name,"null") != 0) {
+      size += 1;
+    } else {
+      break;
+    }
+  }
+  // for (int i = 0; i < size; i++) {
+  //   printf("---\t\t");
+  // }
+  printf("\n");
+  printf("|");
+  for (int i = 0; i < size; i++) {
+    printf("%s\t|\t", dprocess[i].name);
+  }
+  printf("\n");
+  printf("%d\t",dprocess[0].st);
+  for (int i = 1; i < size; i++) {
+    printf("%d\t\t", dprocess[i].st);
+  }
+  printf("%d",dprocess[size-1].ct);
+  printf("\n");
+}
 
 #endif
